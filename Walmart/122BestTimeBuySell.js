5951b6268dad0ee3eb199b0edb17847d61869164
RAJ THAPA <rajthapa@RAJs-MBP.attlocal.net>
2023-12-03 10:10:41 -0500

    buysellstock

---
+++ Walmart/122BestTimeBuySell.js
@@ -0,0 +1,12 @@
+var maxProfit = function(prices) {
+    let maxProfit = 0;
+
+    for (let i = 0; i < prices.length - 1; i++) {
+        if (prices[i] < prices[i + 1]) {
+            // Buy on day i and sell on day i+1
+            maxProfit += prices[i + 1] - prices[i];
+        }
+    }
+
+    return maxProfit;
+};
